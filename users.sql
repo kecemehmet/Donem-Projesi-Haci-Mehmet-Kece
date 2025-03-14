09373cce29b3ee9b5602a16cadd8e0da7fbddee2
Mehmet Keçe <kecemehmet026@gmail.com>
2025-03-14 14:05:12 +0300

    kullanıcı veritabanı oluşturuldu
    böylelikle gelen veriler databaseye kaydedilecek

---
+++ users.sql
@@ -0,0 +1,48 @@
+-- --------------------------------------------------------
+-- Sunucu:                       127.0.0.1
+-- Sunucu sürümü:                8.4.3 - MySQL Community Server - GPL
+-- Sunucu İşletim Sistemi:       Win64
+-- HeidiSQL Sürüm:               12.8.0.6908
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- fitness_db için veritabanı yapısı dökülüyor
+CREATE DATABASE IF NOT EXISTS `fitness_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `fitness_db`;
+
+-- tablo yapısı dökülüyor fitness_db.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `height` float NOT NULL,
+  `weight` float NOT NULL,
+  `bmi` float DEFAULT NULL,
+  `fitness_goal` varchar(50) DEFAULT NULL,
+  `experience_level` varchar(50) DEFAULT NULL,
+  `preferred_exercises` varchar(50) DEFAULT NULL,
+  `workout_days` int DEFAULT NULL,
+  `workout_duration` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- fitness_db.users: ~0 rows (yaklaşık) tablosu için veriler indiriliyor
+DELETE FROM `users`;
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
